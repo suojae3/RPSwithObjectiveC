a5c644240c76db02946285c944158ce1e53c34da
suojae3 <suojae3@gmail.com>
2024-03-18 00:38:43 +0900

    Initial Commit

---
+++ ObjectiveCRPS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCRPS
+//
+//  Created by ㅣ on 3/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
